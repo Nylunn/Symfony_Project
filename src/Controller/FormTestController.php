8fed4d97c55ed1f4a374f23be8d31962d9c2e9f2
Nylun <kawzex@outlook.fr>
2025-02-06 14:41:04 +0100

    [+]Trying to realize USER (Register/Auth/Login)

--- src/Controller/FormTestController.php
+++ src/Controller/FormTestController.php
@@ -8,18 +8,17 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\CategoryType;
 
 final class FormTestController extends AbstractController
 {
-    #[Route('/category/save', name: 'add_category', methods:["GET", "POST"])]
+    #[Route('/category/save', name: 'add_category', methods: ['GET', 'POST'])]
     public function save(Request $request, ManagerRegistry $manager): Response
     {
-        $category = new Category;
-
+        $category = new Category();
         $form = $this->createForm(CategoryType::class, $category);
 
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             $om = $manager->getManager();
             $om->persist($category);
@@ -28,7 +27,8 @@ final class FormTestController extends AbstractController
             return $this->redirectToRoute("home");
         }
 
-        return $this->renderForm('form_test/index.html.twig', [ 'categoryForm' => $form
+        return $this->render('form_test/index.html.twig', [
+            'categoryForm' => $form->createView()
         ]);
     }
 }
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -3,31 +3,18 @@
 namespace App\Form;
 
 use App\Entity\Category;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class TestFormType extends AbstractType
+class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-             ->add('name', TextType::class, [
-            'label' => 'Nom de la catégorie',
-            'attr' => [
-                'class' => 'form-floating',
-                'placeholder' => "Nom de la catégorie"
-            ]
-        ])
-
-         ->add('name', SubmitType::class, [
-            'label' => 'Ajouter',
-            'attr' => [
-                'class' => 'btn btn-primary',
-            ]
-        ])
+             ->add('name')
         ;
     }
 
